6b88139bd53dcde46fc897f3e94712267a83a577
Rakib099 <rakibnabi97@gmail.com>
2023-01-08 18:39:50 +0600

    Verifying JWT middleware added

--- index.js
+++ index.js
@@ -11,8 +11,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = () => {
-    
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message: "Unauthorized Access"});
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if (err) {
+            return res.status(403).send({message: "Forbidden Access"});
+        }
+
+        req.decoded = decoded;
+    });
+
+    next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i9w8jvi.mongodb.net/?retryWrites=true&w=majority`;
@@ -67,7 +82,7 @@ async function run() {
         });
 
         // Booking API
-        app.post('/bookings', async (req, res) => {
+        app.post('/bookings', verifyJWT, async (req, res) => {
             const booking = req.body;
             /* ------------- Checking If Already Booked ---------------- */
             const query = {
